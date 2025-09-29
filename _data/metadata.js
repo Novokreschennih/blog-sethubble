9a8f31ed0666677531a1ab1214894f9e7490d16b
Novokreschennih <Novokreschennih@gmail.com>
2025-09-29 22:50:39 +0800

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,12 +1,12 @@
 export default {
-	title: "Блог SetHubble",
-	url: "https://blog.sethubble.ru/",
-	language: "ru",
-	description: "Новости, обновления и инсайты от команды SetHubble.",
-	currentYear: new Date().getFullYear(),
-	author: {
-		name: "SetHubble",
-		email: "i@inovok.ru",
-		url: "https://sethubble.ru/"
-	}
+    title: "Блог SetHubble",
+    url: "https://blog.sethubble.ru/",
+    language: "ru",
+    description: "Новости, обновления и инсайты от команды SetHubble.",
+    currentYear: new Date().getFullYear(),
+    author: {
+        name: "SetHubble",
+        email: "your-email@example.com",
+        url: "https://sethubble-landing.netlify.app/" // Замените на URL вашего лендинга
+    }
 }
